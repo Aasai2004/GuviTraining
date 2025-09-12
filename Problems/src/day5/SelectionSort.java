221f5992e8e30e1c0447baeab38ddd22b4184cb6
user <user@DESKTOP-7GN9Q6R>
2025-09-12 15:28:59 +0530

    COMMIT

--- Problems/src/day5/SelectionSort.java
+++ Problems/src/day5/SelectionSort.java
@@ -3,7 +3,29 @@ package day5;
 public class SelectionSort {
 	public static void selectionSort(int arr[]) {
 		int n = arr.length;
-		for(int i=0; i<n-12)
+		for(int i=0; i<n-1; i++) {
+			int minIndex = i;
+			for(int j=i+1; j<n; j++) {
+				if(arr[j]<arr[minIndex]) {
+					if(arr[j]<arr[minIndex]) {
+						minIndex = j;
+					}
+				}
+				
+				
+			}
+			int temp = arr[minIndex];
+			arr[minIndex]=arr[i];
+			arr[i]=temp;
+			
+		}
 	}
-
+    public static void main(String[] arg ) {
+    	int arr[] = {64,25,12,22,11};
+    	selectionSort(arr);
+    	System.out.println("Sorted array : ");
+    	for(int num:arr) {
+    		System.out.println(num+ " ");
+    	}
+    }
 }
